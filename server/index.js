363df1f19186a367d9d98710ac6f29f2a6321cb7
kurban1989 <evg.kurban@yandex.ru>
2020-05-22 02:03:50 +0400

    for ngnix routes 4

--- server/index.js
+++ server/index.js
@@ -13,12 +13,12 @@ const articles = require('./routes/articles')
 const comments = require('./routes/comments')
 const cases = require('./routes/cases')
 
-const contentTypes = {
-  '.js': 'application/javascript; charset=utf-8',
-  '.jpeg': 'image/jpeg',
-  '.jpg': 'image/jpeg',
-  '.png': 'image/png'
-}
+// const contentTypes = {
+//   '.js': 'application/javascript; charset=utf-8',
+//   '.jpeg': 'image/jpeg',
+//   '.jpg': 'image/jpeg',
+//   '.png': 'image/png'
+// }
 
 require('./conf/passport')
 
@@ -57,16 +57,16 @@ async function start () {
   //   res.setHeader('Content-Type', contentTypes[path.extname(req.url)])
   //   res.sendFile(path.resolve('/home/customiza.ru/CUSTOMIZA/.nuxt/dist/client/') + req.params.nameFile)
   // })
-  app.get('/_dist/:nameFile', (req, res) => {
-    consola.info({
-      message: `Server request work!`,
-      badge: true
-    })
-    fs.readFile(path.basename(req.url), (_err, data) => {
-      res.writeHead(200, { 'content-type': contentTypes[path.extname(req.url)] })
-      res.end(data)
-    })
-  })
+  // app.get('/_dist/:nameFile', (req, res) => {
+  //   consola.info({
+  //     message: `Server request work!`,
+  //     badge: true
+  //   })
+  //   fs.readFile(path.basename(req.url), (_err, data) => {
+  //     res.writeHead(200, { 'content-type': contentTypes[path.extname(req.url)] })
+  //     res.end(data)
+  //   })
+  // })
 
   app.use('/api/auth', auth)
   app.use('/api/users', users)
